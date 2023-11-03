272292c1a1f6e3f2ebc1d8d60703bbcd67f8cfee
Junho Lee <ckrgkswnsgh@naver.com>
2023-11-03 17:50:22 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/johnpatrickmorgan/FlowStacks", from: "0.3.0"),
+    .package(url: "https://github.com/BlueSignumCorp/FlowStacks", from: "0.3.6"),
     .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.0.0"),
   ],
   targets: [
